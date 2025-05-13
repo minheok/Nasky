596c17eb26375f278beb991ae07ec3068565d9f0
Naska <freenews760@gmail.com>
2025-05-13 12:45:28 +0900

    Fix : show_name_3

--- app.py
+++ app.py
@@ -126,14 +126,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
     
-    
-@app.route("/name", methods=["GET", "POST"])
-def name():
-    if request.method == "POST":
-        session["username"] = request.form["name"]
-        return redirect("/")
-    return render_template("name.html")
-
 
 @app.route("/name", methods=["GET", "POST"])
 def name_input():
